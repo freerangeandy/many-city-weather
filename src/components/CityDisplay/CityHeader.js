bdd8791ac28fdacf3621963d791fbf0ef3238371
Andy Lee <freerangeandy@gmail.com>
2020-07-30 13:27:43 -0400

    swapped out placeholder 'x' for font-awesome icon,

--- src/components/CityDisplay/CityHeader.js
+++ src/components/CityDisplay/CityHeader.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const CityHeader = props => {
 
   return (
     <div className="city-header">
       <div className="city-name"><h1>{props.name}</h1></div>
-      <div>X</div>
+      <div><FontAwesomeIcon icon={ faTimes } /></div>
     </div>
   )
 }
