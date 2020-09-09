90b226031a7e7c7d1d88a4f979dd18b2e3463611
freerangeandy <freerangeandy@gmail.com>
2020-09-09 16:40:48 -0400

    added icon button to city tile to trigger local time display

--- src/components/CityDisplay/CityHeader.js
+++ src/components/CityDisplay/CityHeader.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 
 const CityHeader = props => {
 
   return (
     <div className="city-header">
+      <div onClick={props.showOffsetHandler}>
+        <FontAwesomeIcon className="delete-button" icon={ faClock } />
+      </div>
       <div className="city-name"><h1>{props.name}</h1></div>
       <div onClick={props.deleteHandler}>
         <FontAwesomeIcon className="delete-button" icon={ faTimes } />
