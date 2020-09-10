83a21f45068a9031c1e7d7d6686a71f62360a9af
freerangeandy <freerangeandy@gmail.com>
2020-09-10 18:15:43 -0400

    added label to signify local time selected

--- src/components/CityDisplay/CityHeader.js
+++ src/components/CityDisplay/CityHeader.js
@@ -5,11 +5,13 @@ import { faClock } from '@fortawesome/free-solid-svg-icons'
 
 const CityHeader = props => {
   const timeClass = props.showLocal ? "local-time" : ""
+  const timeLabel = props.showLocal ? "" : "hidden"
 
   return (
     <div className="city-header">
       <div onClick={props.showOffsetHandler}>
         <FontAwesomeIcon className={`icon-button ${timeClass}`} icon={ faClock } />
+        <span className={`local-time-label ${timeLabel}`}>LOCAL</span>
       </div>
       <div className="city-name"><h1>{props.name}</h1></div>
       <div onClick={props.deleteHandler}>
